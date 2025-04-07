02eda62a6c37d19dc6c79c8d22e91c8be4b018ae
DelcioCajila <delciovictorino@icloud.com>
2025-04-07 11:37:24 +0100

    Atualiza templates e URLs do app gerenciar

--- gerenciar/urls.py
+++ gerenciar/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.pagina_inicial, name='pagina_inicial'),
-    path('usuario/cadastrar', views.cadastrar, name='cadastrat'),
+    path('usuario/cadastrar', views.cadastrar, name='cadastrar'),
     path ('usuario/eliminar', views.eliminar, name='eliminar'),
     path ('usuario/editar', views.editar, name='usuario'),
     path ('usuario/atualizar', views.atualizar, name='atualizar')
